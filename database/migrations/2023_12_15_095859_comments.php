c0c69d415bb0989ce09cf403bf8c81e8389e5fee
vampirewolfB <vwberend@gmail.com>
2023-12-15 11:23:10 +0100

    Added the migrations
    Created the migrations. And added 1 test user.

---
+++ database/migrations/2023_12_15_095859_comments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->bigInteger('post_id')->unsigned()->nullable();
+            $table->string('content');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
